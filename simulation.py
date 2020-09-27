d8ec24911c88a106838e7be4415d0ce8eed89277
gamblingrepairman <36687218+gamblingrepairman@users.noreply.github.com>
2020-09-26 21:18:10 -0400

    unpolished final product

--- simulation.py
+++ simulation.py
@@ -11,7 +11,7 @@ class Queue:
         return self.items == []
 
     def enqueue(self, item):
-        self.items.insert(0,item)
+        self.items.insert(0, item)
 
     def dequeue(self):
         return self.items.pop()
@@ -20,7 +20,7 @@ class Queue:
         return len(self.items)
 
     def peek(self):
-        return self.items[len(self.items)-1]
+        return self.items[len(self.items) - 1]
 
 
 class Server:
@@ -51,9 +51,6 @@ class Request:
         self.request_timestamp = int(request_inst[2])
         self.current_request = request_inst
 
-    def get_requesttime(self):
-        return self.request_timestamp
-
     def get_current_request(self):
         return self.current_request
 
@@ -68,18 +65,6 @@ def main():
     parser.add_argument("--servers", type=int, default=1, help="this is for how many servers you wish to use")
     args = parser.parse_args()
 
-    if args.servers == 1:
-        simulateOneServer(args)
-    elif args.servers > 1:
-        simulateManyServers(args)
-    else:
-        print('an error has occurred, servers value must be greater than 0')
-
-
-def simulateOneServer(args):
-    #def simulation(num_seconds, pages_per_minute):
-       #lab_printer = Printer(pages_per_minute)
-
     def queuepull():
         with open(args.fileloc, newline='') as csvfile:
             current_list = []
@@ -91,11 +76,8 @@ def simulateOneServer(args):
     url_queue = Queue()
     queue_list = queuepull()
     queue_list_divided = []
-    while len(queue_list) > 1:  # TODO: need to replace range with better way to get current second
-        request_check = queue_list.pop(0)
-        request = Request(request_check)
-        url_queue.enqueue(request)
-        if url_queue.items[0].current_request[0] == queue_list[0][0]:
+    while 0 < len(queue_list):
+        if url_queue.is_empty() or url_queue.items[0].current_request[0] == queue_list[0][0]:
             request_check = queue_list.pop(0)
             request = Request(request_check)
             url_queue.enqueue(request)
@@ -104,47 +86,76 @@ def simulateOneServer(args):
             for i in range(url_queue.size()):
                 templist.append(url_queue.dequeue())
             queue_list_divided.append(templist)
-    for l in queue_list_divided:
-        print(len(l))
-        for ll in l:
-            print(ll.current_request)
+            request_check = queue_list.pop(0)
+            request = Request(request_check)
+            url_queue.enqueue(request)
+    templist = []
+    for i in range(url_queue.size()):
+        templist.append(url_queue.dequeue())
+    queue_list_divided.append(templist)
+
+    if args.servers == 1:
+        simulateOneServer(queue_list_divided)
+    elif args.servers > 1:
+        simulateManyServers(queue_list_divided, args.servers)
+    else:
+        print('an error has occurred, servers value must be greater than 0')
 
 
+def simulateOneServer(queuelist):
+    url_queue = Queue()
+    current_server = Server()
+    count = len(queuelist)
+    sum_total_wait_times = []
+    for queries in queuelist:
+        for i in queries:
+            url_queue.enqueue(i)
+        if not current_server.busy():
+            waiting_times = []
+            while not url_queue.is_empty():
+                next_task = url_queue.dequeue()
+                waiting_times.append(next_task.wait_time())
+                current_server.start_next(next_task)
+
+            current_server.tick()
+            sum_total_wait_times.append(waiting_times)
+
+            average_wait = sum(waiting_times) / len(queries)
+            print("Average wait %6.2f secs %3d queries remaining." % (average_wait, count))
+            count -= 1
+    sum_total_wait_times = sum(sum_total_wait_times, [])
+    sum_total_wait_times = sum(sum_total_wait_times)
+    print("Average wait %6.2f for single server" % (sum_total_wait_times / len(queuelist)))
 
 
-    """for cs in range(len(queue_list)): #TODO: need to replace range with better way to get current second
-        for queue_count, queue_data in enumerate(queue_list):
-            #print('item is type', type(item), 'lineitem is type', type(lineitem), lineitem)
-            #print(lineitem[0], cs)
-            #print(queue_data[0])
-            if int(queue_data[0]) == count:
-                print('found match')
-                print(count, int(queue_data[0]))
-                request = Request(queue_data)
-                current_queue.enqueue(queue_data)
-                print(current_queue.size())
-            else:
-                #print(count,' did not find a match; adding a count')
-                count+=1
-                break"""
+def simulateManyServers(queuelist, int_server):
+    seq = list(range(0, int_server))
+    seq = {name: Server(name=name) for name in seq}
+    round_robin = itertools.cycle(seq)
+    url_queue = Queue()
+    current_server = seq[next(round_robin)]
+    count = len(queuelist)
+    sum_total_wait_times = []
+    for queries in queuelist:
+        for i in queries:
+            url_queue.enqueue(i)
+        if not current_server.busy():
+            waiting_times = []
+            while not url_queue.is_empty():
+                next_task = url_queue.dequeue()
+                waiting_times.append(next_task.wait_time())
+                current_server.start_next(next_task)
+            current_server = seq[next(round_robin)]
+            current_server.tick()
+            sum_total_wait_times.append(waiting_times)
 
-"""        if (not lab_printer.busy()) and (not print_queue.is_empty()):
-            next_task = print_queue.dequeue()
-        waiting_times.append(next_task.wait_time(current_second))
-        lab_printer.start_next(next_task)
-        lab_printer.tick()
-        average_wait = sum(waiting_times) / len(waiting_times)
-        print("Average Wait %6.2f secs %3d tasks remaining."
-              % (average_wait, print_queue.size()))
+            average_wait = sum(waiting_times) / len(queries)
+            print("Average wait %6.2f secs %3d queries remaining." % (average_wait, count))
+            count -= 1
+    sum_total_wait_times = sum(sum_total_wait_times, [])
+    sum_total_wait_times = sum(sum_total_wait_times)
+    print("Average wait %6.2f for total of %1d servers used" % (sum_total_wait_times / len(queuelist) / int_server,
+                                                                int_server))
 
-        def new_print_task():
-            num = random.randrange(1, 181)
 
-        if num == 180:
-            return True
-        else:
-            return False
-    for i in range(10):
-        simulation(3600, 5)
-"""
 main()
